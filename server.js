20bb596c7efb0b9dc271f6092c9c44d5ed4a2961
Jon Wretlind <jcwretlind@aol.com>
2024-10-25 08:48:29 -0600

    server

--- server.js
+++ server.js
@@ -9,4 +9,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server listening on port ${process.env.PORT || 3000}`);
+  });
